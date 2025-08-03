f2775b8f155e203ee0ed02738d72f4f188c98654
Gabriel-developments <224129097+GabrielDoSaaS@users.noreply.github.com>
2025-08-03 13:31:54 -0300

    Criação da função ConnectToDB

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const connectToDb = require('./src/Db/ConnectToDb');
 const cors = require('cors');
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+connectToDb( )
 
 const port = process.env.PORT || 3000; 
 
