9eac1b2e3011a59fc0d97e5c1e8d0fe7e308e966
Gabriel-developments <224129097+GabrielDoSaaS@users.noreply.github.com>
2025-08-03 15:54:54 -0300

    Atualização no index

--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,29 @@
+// index.js (Atualizado)
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const connectToDb = require('./src/Db/ConnectToDb');
 const cors = require('cors');
 const routes = require('./src/routes/routes');
+const connectToDb = require('./src/Db/ConnectToDb'); 
+const QRCode = require('qrcode');
+const cron = require('node-cron');
 const app = express();
+const PlanController = require('./src/Controllers/PlansController'); 
+
+connectToDb();
 
 app.use(cors());
 app.use(bodyParser.json());
-connectToDb( )
-
 app.use(express.json({
   limit: '50mb'        
 }));
 app.use('/api', routes);
+
+
+cron.schedule('0 0 * * *', () => {
+    console.log('Executando verificação de planos expirados...');
+    PlanController.verificarPlanosExpirados();
+});
 
 const port = process.env.PORT || 3000; 
 
